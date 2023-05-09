cd702c8c5fea0a5f0ad0c91ed9a8af4016e37dad
Antonio Majic <antonio.majich@gmail.com>
2023-05-09 13:16:13 +0200

    first commit

---
+++ edunovajp28.sql
@@ -0,0 +1,48 @@
+drop database if exists edunovajp28;
+
+create database edunovajp28;
+
+use edunovajp28;
+
+create table smjer(
+	sifra int not null primary key auto_increment,
+	naziv varchar(50) not null,
+	cijena decimal(18,2),
+	upisnina decimal(18,2),
+	trajanje int not null,
+	verificiran boolean
+);
+
+create table grupa(
+	sifra int not null primary key auto_increment,
+	naziv varchar(50) not null,
+	smjer int not null,
+	maxpolaznika int,
+	predavac int,
+	datumpocetka datetime
+);
+
+create table predavac(
+	sifra int not null primary key auto_increment,
+	iban varchar(50),
+	osoba int not null
+);
+
+create table polaznik(
+	sifra int not null primary key auto_increment,
+	brojugovora varchar(20),
+	osoba int not null
+);
+
+create table osoba(
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	oib char(11),
+	email varchar(100)
+); 
+
+create table clan(
+	grupa int not null,
+	polaznik int not null
+);
