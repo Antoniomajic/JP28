b7cb838032f71ff6510c72765fb4c7f08f84bc1e
Antoniomajic <antonio.majich@gmail.com>
2023-05-15 15:22:43 +0200

    Added -taksisluzba.sql- database & ER diagram of it

---
+++ taksisluzba.sql
@@ -0,0 +1,42 @@
+#drop database if exists taksisluzba;
+
+create database taksisluzba;
+use taksisluzba;
+
+create table vozilo(
+	id int not null primary key auto_increment,
+	marka varchar(50) not null
+);
+
+create table vozac(
+	id int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	oib char(11),
+	vozilo int
+);
+
+create table voznja(
+	id int not null primary key auto_increment,
+	vozac int not null
+);
+
+create table putnik(
+	id int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null
+);
+
+create table voznja_putnik(
+	id int not null primary key auto_increment,
+	putnik int not null,
+	voznja int not null,
+	trajanje int,
+	cijena decimal(18,2)
+);
+
+alter table vozac add foreign key (vozilo) references vozilo (id);
+alter table voznja_putnik add foreign key (putnik) references putnik (id);
+alter table voznja_putnik add foreign key (voznja) references voznja (id);
+alter table voznja add foreign key (vozac) references vozac (id);
+
