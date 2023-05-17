15be8dda84562797dc8aefbd2a4160b32ebaf9fb
Antoniomajic <antonio.majich@gmail.com>
2023-05-17 14:58:08 +0200

    Modified -korisnik- table & inserted value into -djelatnica- table

--- salonzauljepsavanje.sql
+++ salonzauljepsavanje.sql
@@ -1,6 +1,7 @@
--- create database salonzauljepsavanje;
-
--- use salonzauljepsavanje;
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\javaprogrameri\dz1\salonzauljepsavanje.sql
+drop database if exists salonzauljepsavanje;
+create database salonzauljepsavanje;
+use salonzauljepsavanje;
 
 create table djelatnica(
 	sifra int not null primary key auto_increment,
@@ -11,7 +12,7 @@ create table djelatnica(
 
 create table korisnik(
 	sifra int not null primary key auto_increment,
-	spol bit not null,
+	spol varchar(10) not null,
 	usluga int not null
 );
 
@@ -34,4 +35,10 @@ create table termin(
 alter table termin add foreign key (usluga) references usluga (sifra);
 alter table termin add foreign key (korisnik) references korisnik (sifra);
 alter table termin add foreign key (djelatnica) references djelatnica (sifra);
+
+insert into djelatnica (sifra,ime,prezime,oib)
+values 
+(null,'Marija','Juriša',null),
+(null,'Vanja','Radman',null),
+(null,'Mirjana','Obradović',null);
 
