0b10f6c2f12a63477c820b259e47b6987b21631d
Antoniomajic <antonio.majich@gmail.com>
2023-05-23 19:44:41 +0200

    Modified TaxiService

--- TaxiService.sql
+++ TaxiService.sql
@@ -1,42 +1,75 @@
-#drop database if exists taksisluzba;
+-- c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\javaprogrameri\dz1\TaxiService.sql
 
-create database taksisluzba;
-use taksisluzba;
+drop database if exists TaxiService;
+create database TaxiService;
+use TaxiService;
 
-create table vozilo(
+create table vehicle(
 	id int not null primary key auto_increment,
-	marka varchar(50) not null
+	brand varchar(50) not null,
+	registration varchar (50),
+	max_passengers int
 );
 
-create table vozac(
+create table driver(
 	id int not null primary key auto_increment,
-	ime varchar(50) not null,
-	prezime varchar(50) not null,
-	oib char(11),
-	vozilo int
+	firstname varchar(50) not null,
+	lastname varchar(50) not null,
+	id_number varchar(11),
+	vehicle int not null
 );
 
-create table voznja(
+create table service(
 	id int not null primary key auto_increment,
-	vozac int not null
+	driver int not null
 );
 
-create table putnik(
+create table passenger(
 	id int not null primary key auto_increment,
-	ime varchar(50) not null,
-	prezime varchar(50) not null
+	phonenumber varchar (50) not null
 );
 
-create table voznja_putnik(
+create table passenger_service(
 	id int not null primary key auto_increment,
-	putnik int not null,
-	voznja int not null,
-	trajanje int,
-	cijena decimal(18,2)
+	passenger int not null,
+	service int not null,
+	duration int,
+	price decimal (18,2)
 );
 
-alter table vozac add foreign key (vozilo) references vozilo (id);
-alter table voznja_putnik add foreign key (putnik) references putnik (id);
-alter table voznja_putnik add foreign key (voznja) references voznja (id);
-alter table voznja add foreign key (vozac) references vozac (id);
+alter table driver add foreign key (vehicle) references vehicle(id);
+alter table passenger_service add foreign key (passenger) references passenger(id);
+alter table passenger_service add foreign key (service) references service(id);
+alter table service add foreign key (driver) references driver(id);
+
+insert into vehicle (id,brand,registration,max_passengers)
+	values 
+			(null,'Audi A3','OS-457-SL','3'),
+			(null,'Škoda Octavia','ZG-364-RT','4'),
+			(null,'Renault Trafic','DJ-7888-PM','7');
+
+insert into driver (id,firstname,lastname,id_number,vehicle)
+	values 
+			(null,'Ante','Dujmić','05761983764',1),
+			(null,'Domagoj','Peršić','35789145728',2),
+			(null,'Vanja','Časnić','65748957810',3);
+
+insert into passenger (id,phonenumber) 
+	values
+			(null,'09758614785'),
+			(null,'09863585585'),
+			(null,'09148854122'),
+			(null,'09147833256'),
+			(null,'+3877849986'),
+			(null,'09754877786');
+
+insert into service (id,driver)
+	values
+			(null,'1'),
+			(null,'2'),
+			(null,'3');
+
+insert into passenger_service (id,passenger,service,duration,price)
+	values
+			(null,'1','1','50','5');
 
