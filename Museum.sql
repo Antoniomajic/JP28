a4f3acd4b1cc59a6290b9d769c6c921bbdaf3605
Antoniomajic <antonio.majich@gmail.com>
2023-05-23 18:33:46 +0200

    Modified Museum

--- Museum.sql
+++ Museum.sql
@@ -1,36 +1,62 @@
-# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\javaprogrameri\dz1\BeautySalon.sql
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\javaprogrameri\dz1\Museum.sql
 
 drop database if exists Museum;
 create database Museum;
 use Museum;
 
-create table kustos(
-	sifra int not null primary key auto_increment,
-	ime varchar(50) not null,
-	prezime varchar(50) not null,
-	email varchar(100)
+create table curator(
+	id int not null primary key auto_increment,
+	firstname varchar(50) not null,
+	lastname varchar(50) not null,
+	phone_number varchar(100)
 );
 
-create table sponzor(
-	sifra int not null primary key auto_increment,
-	ime varchar(50) not null
+create table sponsor(
+	id int not null primary key auto_increment,
+	name varchar(50) not null
 );
 
-create table djelo(
-	sifra int not null primary key auto_increment,
-	naziv varchar(50) not null,
-	opis varchar(100),
-	izlozba int not null
+create table exhibit(
+	id int not null primary key auto_increment,
+	name varchar(50) not null,
+	description varchar(100),
+	exhibition int not null
 );
 
-create table izlozba(
-	sifra int not null primary key auto_increment,
-	naziv varchar(100),
-	sponzor int not null,
-	kustos int not null,
-	djelo int not null
+create table exhibition(
+	id int not null primary key auto_increment,
+	name varchar(100),
+	sponsor int not null,
+	curator int not null,
+	exhibit int not null
 );
 
-alter table djelo add foreign key (izlozba) references izlozba (sifra); 
-alter table izlozba add foreign key (sponzor) references sponzor (sifra);
-alter table izlozba add foreign key (kustos) references kustos (sifra);
+alter table exhibit add foreign key (exhibition) references exhibition (id); 
+alter table exhibition add foreign key (sponsor) references sponsor (id);
+alter table exhibition add foreign key (curator) references curator (id);
+
+insert into curator (id,firstname,lastname,phone_number)
+	values
+			(null,'Nenad','Žižić','0917458254'),
+			(null,'Marko','Šarić','0974573256'),
+			(null,'Gabrijela','Vinković','0986474117');
+
+insert into sponsor (id,name)
+	values
+			(null,'Konzum'),
+			(null,'Ured Predsjednika RH'),
+			(null,'Veleposlanstvo Kraljevine Španjolske u RH');
+
+insert into exhibition (id,name,sponsor,curator)
+	values
+			(null,'Suvremena umjetnost',1,3),
+			(null,'Sisak u 15.-tom stoljeću',2,1),
+			(null,'Alternativna abstrakcija',3,2);
+
+insert into exhibit (id,name,description,exhibition)
+	values
+			(null,'Vrtlog','Slika Andrije Palića iz 1997.g.',2),
+			(null,'Kaciga','Srednjovjekovna vojna oprema',1),
+			(null,'Uže slobode','Umjetnički eksponat nepoznate upotrebe',3);
+
+update sponsor set name='British Embassy in Republic of Croatia' where id=3;
