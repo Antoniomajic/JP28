bcd19f387ec375616d1ca6c9e32555c091c2cf06
Antoniomajic <antonio.majich@gmail.com>
2023-06-16 19:45:00 +0200

    Practice 2 - Hello World

---
+++ Java/eclipse/Practice02/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Korisnik
+ *
+ */
+module Practice02 {
+}
---
+++ Java/eclipse/Practice02/src/practice02/Practice01.java
@@ -0,0 +1,12 @@
+package practice02;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		System.out.println("Third Hello World practice");
+		
+		
+
+	}
+
+}
