3631aa00878dab06e5a0f75a141d671e75abf2c8
Antoniomajic <antonio.majich@gmail.com>
2023-06-20 11:55:17 +0200

    Created Edunova03 project

---
+++ Java/eclipse/Edunova03/src/edunova/E03UvjetnoGrananjeIf.java
@@ -0,0 +1,26 @@
+package edunova;
+
+public class E03UvjetnoGrananjeIf {
+	public static void main(String[] args) {
+		boolean uvjet = true;
+		
+		if(uvjet) {
+			System.out.println("1");
+		}
+		
+		else {
+			System.out.println("2");
+		}
+		
+		int i=1;
+		if(i>0) {
+			System.out.println("3");
+		}
+		
+		if(i==0)
+			System.out.println("4");
+			System.out.println("5");
+	}
+}
+
+//this is comment
---
+++ Java/eclipse/Edunova03/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Korisnik
+ *
+ */
+module Edunova03 {
+	requires java.desktop;
+}
